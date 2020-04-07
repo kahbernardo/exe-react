a965d3c807e04eb8152bdd08107d1447c4f8b70d
kahbernardo <kaiquebernardo02@gmail.com>
2020-04-07 02:31:49 -0300

    aula 13

--- src/components/PrimeiroComponente.jsx
+++ src/components/PrimeiroComponente.jsx
@@ -4,6 +4,12 @@ function primeiro (){
     return <h1>Componente!</h1>
 }
 
+
+
+// export default ()=>
+//      <h1>Componente arrow!!</h1>
+
+
 // export default function(){
 //     return <h1>Componente!!</h1>
 // }
