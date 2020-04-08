7fdf07eb03884f6cff95ef41d490df04c1090d81
kahbernardo <kaiquebernardo02@gmail.com>
2020-04-08 02:22:34 -0300

    aula 18

---
+++ src/components/FamiliaBernardo.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import Membro from './Membro'
+
+export default props =>
+    <>
+        <Membro nome="Kaique" sobrenome="Bernardo"/>
+        <Membro nome="Laís" sobrenome="Bernardo"/>
+    </>
--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom'
 import Primeiro from './components/PrimeiroComponente'
 import A,{CompB as B} from './components/MultiComp'
 import MultiElementos from './components/MultiElementos'
+import Familia from './components/FamiliaBernardo'
 
 const elemento = document.getElementById('root')
 
 ReactDOM.render(
     <div>
-        {/* <MultiElementos/> */}
+        <Familia/>
+        {/* <MultiElementos/>
         <A valor="Olá A"/>
         <B valor="Olá B"/>
-        {/* <Primeiro valor="Propriedade texto" funcao={Math.pow(2,8)} var={0}/> */}
+        <Primeiro valor="Propriedade texto" funcao={Math.pow(2,8)} var={0}/> */}
     </div>,elemento)
