1b7a1db3dabfc12757d45b6fa33e048eedc4a1ea
kahbernardo <kaiquebernardo02@gmail.com>
2020-04-08 01:55:52 -0300

    aula 16

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Primeiro from './components/PrimeiroComponente'
+import {CompA,CompB} from './components/MultiComp'
 
 const elemento = document.getElementById('root')
 
 ReactDOM.render(
     <div>
-        <Primeiro valor="Propriedade texto" funcao={Math.pow(2,8)} var={0}/>
+        <CompA valor="Olá A"/>
+        <CompB valor="Olá B"/>
+        {/* <Primeiro valor="Propriedade texto" funcao={Math.pow(2,8)} var={0}/> */}
     </div>,elemento)
